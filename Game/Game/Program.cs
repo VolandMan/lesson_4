4a92e1c6acac69b9c3262d79e8026974eef194a9
Voland <maloyvladis@gmail.com>
2019-09-25 22:08:12 +0300

    First Lesson_4

---
+++ Game/Game/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] array = { '@', '-', '-', '*', '-', '+', '-', '-', '-', '-' };
+            int kol=0;
+            char c;
+            int DogHealth = 100;
+            Console.WriteLine("Начальный массив");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Start");
+           
+            // перемещаю собаку по массиву
+            while (kol < 9)
+            {
+                //Console.Clear();
+                Console.WriteLine("Нажми (1) чтобы сделать ход собакой");
+                c = Char.Parse(Console.ReadLine());
+                if (c == '1')
+                {
+                    if (DogHealth <= 0) { Console.WriteLine("Собака больше не может ходить!!! GAME OVER"); break; }
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                     // проверка элементов массива на бомбу и аптечку
+                        array[kol] = '-';
+                        if (array[kol + 1] =='*')
+                           {
+                            Console.WriteLine("БОМБА!!, но собака еще жива");
+                            DogHealth -= 40;
+                           }
+                        if (array[kol + 1] == '+') 
+                            {
+                            Console.WriteLine("Наш Тузик востановил часть здоровья");
+                            DogHealth +=40;
+                            }
+                        array[kol + 1] = '@';
+                       // Вывод массива после изменений
+                        Console.Write(array[i]); 
+                      
+                    }
+                 }
+                DogHealth -=10;
+                kol++;
+                Console.WriteLine($" Здоровье собаки= {DogHealth}");
+           
+            }
+            Console.WriteLine($" Собака нагулялась:)");
+        }
+        
+    }
+}
+
+        
+
---
+++ copyarray/CopyMass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CopyMass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Num = new Random();
+            int n = 4;
+            int k = 12;
+            int[] mass = new int[n];
+            int[] mass2 = new int[k+n];
+
+            for ( int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = Num.Next(30,100);
+                Console.Write(mass[i]  + " ");
+            }
+            Console.WriteLine();
+            for (int j = 0; j < mass2.Length; j++)
+            {
+                mass2[j] = Num.Next(1,20); ;
+                Console.Write(mass2[j] + " ");
+            }
+            Console.WriteLine();
+            Array.Copy(mass ,0, mass2,6, mass .Length);
+
+            for (int j = 0; j < mass2.Length; j++)
+            {
+                Console.Write(mass2[j] + " ");
+            }
+
+
+        } 
+        
+    }
+}
